cdf9938f72e5738e33d142a601e49530264ae6a6
ClaudioerBoncio <bnccld05r03c933d@iisbadoni.edu.it>
2024-05-12 20:27:14 +0200

    the code send the video whithout download it on the host's -cb

--- botTelegram.py
+++ botTelegram.py
@@ -2,6 +2,7 @@
 
 import telebot
 from pytube import YouTube
+import requests
 from io import BytesIO
 
 # Funzione per ottenere il token dal file token_bot.txt
@@ -46,13 +47,17 @@ def echo_message(message):
             # Scarica il video
             yt = YouTube(message.text)
             stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-            video = stream.download()
-            with open(video, 'rb') as video_file:
-                bot.send_video(message.chat.id, video_file)
-            # Invia il messaggio di conferma con la risoluzione del video
-            bot.reply_to(message, f"Video scaricato con successo!\nRisoluzione: {stream.resolution}")
+            video_url = stream.url
+            # Carica il video direttamente su Telegram
+            video_data = requests.get(video_url)
+            if video_data.status_code == 200:
+                bot.send_video(message.chat.id, video_data.content)
+                # Invia il messaggio di conferma con la risoluzione del video
+                bot.reply_to(message, f"Video inviato con successo!\nRisoluzione: {stream.resolution}")
+            else:
+                bot.reply_to(message, "Impossibile scaricare il video da YouTube al momento.")
         except Exception as e:
-            bot.reply_to(message, f"Si è verificato un errore durante il download del video: {str(e)}")
+            bot.reply_to(message, f"Si è verificato un errore durante il caricamento del video: {str(e)}")
     else:
         bot.reply_to(message, """\
 Ciao, sono Il SucchiaVideoBot.
